23a939c607757adeedd22037f919f14f385753de
Fine Guy <122565078+Fine-Guy-21@users.noreply.github.com>
2026-02-23 14:52:57 +0300

    feat: added contact, about us , donation and donation-detail pages

---
+++ assets/js/donation.js
@@ -0,0 +1,94 @@
+const causes = [
+  {
+    id: 1,
+    title: "Helping the homeless during hopeless",
+    image: "assets/images/donation/Cause1.png",
+    raised: 49500,
+    goal: 55000
+  },
+  {
+    id: 2,
+    title: "Fighting hunger with food distribution drives",
+    image: "assets/images/donation/cause2.png",
+    raised: 49500,
+    goal: 55000
+  },
+  {
+    id: 3,
+    title: "Providing medical aid to underserved areas",
+    image: "assets/images/donation/cause3.png",
+    raised: 49500,
+    goal: 55000
+  },
+  {
+    id: 4,
+    title: "Crisis Hardship Grants for Widows & Elders",
+    image: "assets/images/donation/cause4.png",
+    raised: 49500,
+    goal: 55000
+  },
+  {
+    id: 5,
+    title: "Aiding the homeless when things seem hopeless",
+    image: "assets/images/donation/cause5.png",
+    raised: 49500,
+    goal: 55000
+  },
+  {
+    id: 6,
+    title: "Emergency Nutrition for War-Displaced Families",
+    image: "assets/images/donation/cause6.png",
+    raised: 49500,
+    goal: 55000
+  }
+];
+
+const container = document.querySelector(
+  ".donation-hero-section-donation-causes"
+);
+
+function formatCurrency(amount) {
+  return `$${amount.toLocaleString()}`;
+}
+
+function createCard(cause) {
+  const percentage = Math.round((cause.raised / cause.goal) * 100);
+
+  return `
+    <div class="donation-card">
+      
+      <div class="donation-card-image">
+        <img src="${cause.image}" alt="${cause.title}">
+      </div>
+
+      <div class="donation-card-status">
+        <div class="donation-card-status-top">
+          <h3>Donation</h3>
+          <p>${percentage}%</p>
+        </div>
+
+        <div class="donation-card-status-progress">
+          <div class="progress-bar">
+            <div class="progress-fill" style="width:${percentage}%"></div>
+          </div>
+        </div>
+
+        <div class="donation-card-status-bottom">
+          <h2>Raised ${formatCurrency(cause.raised)}</h2>
+          <h2>Goal ${formatCurrency(cause.goal)}</h2>
+        </div>
+      </div>
+
+      <div class="donation-card-description">
+        ${cause.title}
+      </div>
+
+      <button class="donate-btn" onClick=(${cause.id})>
+        Donate Now ↗
+      </button>
+
+    </div>
+  `;
+}
+
+container.innerHTML = causes.map(createCard).join("");
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,15 @@ const navMenu = document.getElementById("navMenu");
 const overlay = document.getElementById("overlay");
 const closeBtn = document.getElementById("closeBtn");
 const navLinks = document.querySelectorAll("#navMenu a");
+const donateButton = document.getElementById("dontate-button");
 
+// donate button
+
+donateButton.addEventListener("click", () => {
+  window.location.href = "donation.html";
+});
+
+// mobile responsive drawer
 function openDrawer() {
   navMenu.classList.add("active");
   overlay.classList.add("active");
@@ -21,3 +29,26 @@ overlay.addEventListener("click", closeDrawer);
 navLinks.forEach((link) => {
   link.addEventListener("click", closeDrawer);
 });
+
+// Highlight active nav link
+const currentPath = window.location.pathname.split("/").pop();
+const navLinksActive = document.querySelectorAll("#navMenu a");
+navLinksActive.forEach((link) => {
+  if (link.getAttribute("href") === currentPath) {
+    link.classList.add("active");
+  }
+});
+
+function displayDimensions() {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+
+  const h2 = document.createElement("h2");
+  h2.innerText = `Current Width: ${width}px, Current Height: ${height}px`;
+
+  document.querySelector(".debug").appendChild(h2);
+}
+
+displayDimensions();
+// 1580 px
+// 1280 px
